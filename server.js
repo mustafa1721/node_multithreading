d9ca451fa0670672fa3ac9b4b9f5e6f6c68c60fb
Mustafa <mustafasayyed1721@gmail.com>
2024-08-06 15:42:09 +0530

    commit

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-const express = require('express')
-const { Worker } = require('worker_threads')
-const port = 3000
-const app = express()
-app.use(express())
+const express = require("express");
+const { Worker } = require("worker_threads");
+const port = 3000;
+const app = express();
+app.use(express());
 
-app.get('/non-blocking', (req, res) => {
-    res.status(200).send("Non blocking page")
-})
+app.get("/non-blocking", (req, res) => {
+  res.status(200).send("Non blocking page");
+});
 
-app.get('/blocking', (req, res) => {
+app.get("/blocking", (req, res) => {
+  const worker = new Worker("./worker.js");
 
-    const worker = new Worker('./worker.js')
+  worker.on("message", (data) => {
+    res.status(200).send(`result is ${data}`);
+  });
 
-    worker.on("message", (data) => {
-        res.status(200).send(`result is ${data}`)
-    });
-
-    worker.on("error", (error) => {
-        res.status(400).send(`An error occured:${error}`)
-    });
-
-})
-
-
+  worker.on("error", (error) => {
+    res.status(400).send(`An error occured:${error}`);
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
--- server_one_worker.js
+++ server_one_worker.js
@@ -1,41 +1,38 @@
-const express = require('express')
-const { Worker, workerData } = require('worker_threads')
-const port = 3000
-const app = express()
-const THREAD_COUNT = 2
+const express = require("express");
+const { Worker, workerData } = require("worker_threads");
+const port = 3000;
+const app = express();
+const THREAD_COUNT = 2;
 
-app.use(express())
+app.use(express());
 
-app.get('/non-blocking', (req, res) => {
-    res.status(200).send("Non blocking page")
+app.get("/non-blocking", (req, res) => {
+  res.status(200).send("Non blocking page");
 });
 
 function createWorker() {
-    return new Promise((resolve, reject) => {
-        const worker = new Worker('./one_worker.js', {
-            workerData: { thread_count: THREAD_COUNT }
-        })
-        worker.on('message', (data) => {
-            resolve(data)
-        })
-        worker.on('error', (error) => {
-            reject(error)
-        })
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./one_worker.js", {
+      workerData: { thread_count: THREAD_COUNT },
     });
+    worker.on("message", (data) => {
+      resolve(data);
+    });
+    worker.on("error", (error) => {
+      reject(error);
+    });
+  });
 }
-app.get('/blocking', async (req, res) => {
-
-    const workerPromises = [];
-    for (let i = 0; i < THREAD_COUNT; i++) {
-        workerPromises.push(createWorker())
-    }
-    const thread_results = await Promise.all(workerPromises)
-    const total = thread_results[0] + thread_results[1]
-    res.status(200).send(`result is ${total}`)
-})
-
-
+app.get("/blocking", async (req, res) => {
+  const workerPromises = [];
+  for (let i = 0; i < THREAD_COUNT; i++) {
+    workerPromises.push(createWorker());
+  }
+  const thread_results = await Promise.all(workerPromises);
+  const total = thread_results[0] + thread_results[1];
+  res.status(200).send(`result is ${total}`);
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
